43e7754d1bbc25478f619af328967f3cadf049f4
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-03-29 15:58:39 +0200

    init commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,5 @@
+{{-- CREO LA HOMEPAGE DEL SITO --}}
+{{-- import di tutto cio' che e' legato a vite --}}
+@vite('resources/js/app.js')
+{{-- import delle immagini --}}
+{{-- <img class="my-1" src="{{ Vite::asset('resources/img/logo.png') }}" alt=""> --}}
